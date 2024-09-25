36e1497a1de312fea949226507a443a978da1668
rc1021 <rc.701021@gmail.com>
2024-09-25 16:53:48 +0800

    新增eui的環境

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@ ENV_TYPE=$1
 
 # Help
 if [ "$ENV_TYPE" = "--help" ]; then
-    echo "Usage: ./start.sh [ez100|indo|upup] [rebuild]"
-    echo "Usage: ./start.sh down [ez100|indo]"
+    echo "Usage: ./start.sh [ez100|indo|ph-jp|reports|vn|hk-indo] [rebuild]"
+    echo "Usage: ./start.sh down [ez100|indo|ph-jp|reports|vn|hk-indo]"
     exit 1
 fi
 
@@ -15,11 +15,17 @@ if [ "$ENV_TYPE" = "down" ]; then
 
     PROJECT=$2
         if [ "$PROJECT" = "ez100" ]; then
-        command="docker-compose --env-file ./.env.ez100 down"
+        command="docker compose --env-file ./.env.ez100 down"
     elif [ "$PROJECT" = "indo" ]; then
-        command="docker-compose --env-file ./.env.indo down"
+        command="docker compose --env-file ./.env.indo down"
     elif [ "$PROJECT" = "ph-jp" ]; then
-        command="docker-compose --env-file ./.env.ph.japan down"    
+        command="docker compose --env-file ./.env.ph.japan down"    
+    elif [ "$PROJECT" = "reports" ]; then
+        command="docker compose --env-file ./.env.reports down"
+    elif [ "$PROJECT" = "vn" ]; then
+        command="docker compose --env-file ./.env.vn down"
+    elif [ "$PROJECT" = "hk-indo" ]; then
+        command="docker compose --env-file ./.env.hk-indo down"
     else
         echo "錯誤：未知的專案類型。"
         exit 1
@@ -33,16 +39,25 @@ REBUILD=$2
 # 根據參數執行相應的命令
 if [ "$ENV_TYPE" = "ez100" ]; then
     echo "正在為 ez100 環境啟動容器..."
-    command="docker-compose --env-file ./.env.ez100 up -d redis mysql phpmyadmin nginx laravel-echo-server laravel-horizon"
+    command="docker compose --env-file ./.env.ez100 up -d redis mysql phpmyadmin nginx laravel-echo-server laravel-horizon"
 elif [ "$ENV_TYPE" = "indo" ]; then
     echo "正在為 indo-money 環境啟動容器..."
-    command="docker-compose --env-file ./.env.indo up -d redis mysql phpmyadmin nginx"
+    command="docker compose --env-file ./.env.indo up -d redis mysql phpmyadmin nginx mailpit"
 elif [ "$ENV_TYPE" = "ph-jp" ]; then
     echo "正在為 ph-money-japan 環境啟動容器..."
-    command="docker-compose --env-file ./.env.ph.japan up -d redis mysql phpmyadmin nginx"
+    command="docker compose --env-file ./.env.ph.japan up -d redis mysql phpmyadmin nginx mailpit"
+elif [ "$ENV_TYPE" = "reports" ]; then
+    echo "正在為 reports 環境啟動容器..."
+    command="docker compose --env-file ./.env.reports up -d redis mysql phpmyadmin nginx mailpit"
+elif [ "$ENV_TYPE" = "vn" ]; then
+    echo "正在為 vn 環境啟動容器..."
+    command="docker compose --env-file ./.env.vn up -d redis mysql phpmyadmin nginx  mailpit"
 elif [ "$ENV_TYPE" = "upup" ]; then
     echo "正在為 upup 環境啟動容器..."
-    command="docker-compose --env-file ./.env.ez100 up -d edis mysql phpmyadmin nginx php-worker"
+    command="docker compose --env-file ./.env.ez100 up -d redis mysql phpmyadmin nginx php-worker"
+elif [ "$ENV_TYPE" = "hk-indo" ]; then
+    echo "正在為 hk-indo 環境啟動容器..."
+    command="docker compose --env-file ./.env.hk-indo up -d redis mysql phpmyadmin nginx mailpit"
 else
     echo "錯誤：未知的環境類型。"
     exit 1
